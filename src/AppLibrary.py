9b3ee6bb3c37783932659f163a6a674aaf198323
tfhuhtal <tuomas.huhtala@icloud.com>
2023-11-27 18:53:33 +0200

    test asset class

---
+++ src/AppLibrary.py
@@ -0,0 +1,34 @@
+from console_io import ConsoleIO
+from referencehandler import ReferenceHandler
+
+import re
+
+class AppLibrary:
+    def __init__(self):
+        self._io = ConsoleIO()
+
+        self._app = ReferenceHandler(self._io)
+
+    def input(self, value):
+        self._io.add_input(value, True)
+
+    def output_should_contain(self, value):
+        outputs = self._io.outputs
+
+        splitted = []
+        for out in outputs:
+            splitted.append(re.split(r'\W+', out))
+
+        x = False
+        for split in splitted:
+            if value in split:
+                x = True
+        
+        if not x:
+            raise AssertionError(
+                    f"Output \"{value}\" is not in {str(splitted)}"
+                    )
+
+    def run_application(self):
+        self._app.run()
+
