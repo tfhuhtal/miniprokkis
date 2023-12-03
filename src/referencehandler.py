7eb35ed0c20208470b59e551f8fec04b888c2be6
tfhuhtal <tuomas.huhtala@icloud.com>
2023-12-03 12:40:57 +0200

    adding restructured

--- src/referencehandler.py
+++ src/referencehandler.py
@@ -1,7 +1,5 @@
 import re
 from converter import Converter
-from reference import Reference
-from reference_types import ReferenceTypes
 from services.print.printservice import Printservice
 from services.keyhandler import Keyhandler
 from services.delete import DeleteService
@@ -12,7 +10,6 @@ class ReferenceHandler:
         self.converter = converter if converter else Converter(
             "savedreferences.json")
         self.io = io
-        self.reference_types = ReferenceTypes("src/assets/source_types.json")
         self.printter = Printservice(self.converter)
         self.keyhandler = Keyhandler(self.converter)
         self.deletehandler = DeleteService(self.converter)
@@ -28,120 +25,13 @@ class ReferenceHandler:
         self.io.write("4 - Tulosta bibtex -lähdelista")
 
     #siirrä
-    def input_ref_key(self, existing_keys: list):
-        while True:
-            if len(self.io.inputs) == 0:
-                self.io.add_input(
-                    "\nLähteen avain: ('ENTER' peruaksesi toiminto) ")
-            input = self.io.read()
-
-            if input == "":
-                return 0
-
-            if input in existing_keys:
-                self.io.write(
-                    f"\nAvain '{input}' on jo käytössä. Käytä jotain toista avainta.")
-                continue
-
-            if re.match("^[A-Za-z0-9_-]+$", input):
-                return input
-            self.io.write(
-                f"\nAvain saa sisältää vain kirjaimia a-z ja numeroita.")
-            continue
+    
 
     #siirrä
-    def input_ref_type(self, types: list):
-        self.io.write(
-            f"\nMahdolliset lähdetyypit: {self._string_of_types(types)}")
-
-        while True:
-            if len(self.io.inputs) == 0:
-                self.io.add_input(
-                    "\nLähteen tyyppi: ('exit' peruaksesi toiminto) ")
-            input = self.io.read()
-
-            if input == "":
-                self.io.write("\nKenttä ei voi olla tyhjä")
-                continue
-
-            if input == "exit":
-                return 0
-
-            if input not in types:
-                self.io.write("\nTyyppi ei käytössä")
-                continue
-
-            return input
+    
 
     #siirrä
-    def input_ref_fields(self, data: dict, fields: dict, field_type: str):
-        self.io.write(f"\n{field_type} kentät: ('exit' peruaksesi toiminto) ")
-
-        for field in fields[field_type]:
-            while True:
-                if len(self.io.inputs) == 0:
-                    self.io.add_input(f"{field : >12}: ")
-
-                input = self.io.read()
-
-                if input == "exit":
-                    return 0
-                
-                if input == "" and field_type == "Pakolliset":
-                    self.io.write("\nKenttä ei voi olla tyhjä")
-                    continue
-                elif input == "" and field_type == "Vapaaehtoiset":
-                    break
-                
-                validate_result = self.validate_input(input, field)
-                if validate_result != 1:
-                    self.io.write(f"Virhe: ({validate_result})")
-                    continue
-
-                data["fields"][field] = input
-                break
-            
-        return 1
-
-    #siirrä
-    def add(self):
-        data = {}
-
-        # Kysy lähteen avainta
-        existing_keys = self.keyhandler.get_keys()
-
-        input = self.input_ref_key(existing_keys)
-        if input == 0:
-            self.io.write("\nToiminto peruttu")
-            return 0
-        data["key"] = input
-
-        # Kysy lähteen tyyppiä, jonka pitää löytyä source_types.json
-        # tiedostosta
-        types = self.reference_types.get_types()
-        input = self.input_ref_type(types)
-        if input == 0:
-            self.io.write("\nToiminto peruttu")
-            return 0
-        data["type"] = input
-
-        # Hae pakolliset ja vapaaehtoiset kentät lähdetyypin perusteella
-        data["fields"] = {}
-        fields = self.reference_types.get_fields(data["type"])
-
-        # Kysy pakolliset kentät
-        if self.input_ref_fields(data, fields, "Pakolliset") == 0:
-            self.io.write("\nToiminto peruttu")
-            return 0
-
-        # Kysy vapaaehtoiset kentät. Tyhjä input ohittaa kentän
-        if self.input_ref_fields(data, fields, "Vapaaehtoiset") == 0:
-            self.io.write("\nToiminto peruttu")
-            return 0
-
-        new_reference = Reference(data)
-        self.adder.add_reference(new_reference)
-        self.io.write("\nLähde lisätty.")
+    
 
     #siirrä
     def list_references(self, alphabetical):
@@ -182,7 +72,7 @@ class ReferenceHandler:
             if command == "0":
                 break
             if command == "1":
-                self.add()
+                self.adder.add()
             if command == "2":
                 self.list_references(False)
             if command == "2 -a":
@@ -195,11 +85,6 @@ class ReferenceHandler:
                 self.info()
 
     #siirrä
-    def _string_of_types(self, types):
-        string = ""
-        for type in types:
-            string += type + " "
-        return string
 
     #siirrä
     def print_bibtex(self):
@@ -208,10 +93,3 @@ class ReferenceHandler:
         self.io.write(data)
 
     #siirrä
-    def validate_input(self, input, field: str):
-        if field == "year" and not re.match(r"^\d{4}$", input):
-            return "year-kentän tulee olla 4 numeroinen"
-        if field == "pages" and not re.match(
-                r"^\d+(--\d+)?(, \d+(--\d+)?)*$", input):
-            return "pages-kentän tulee olla muotoa '1' tai '1--5' tai '1, 3--5, 7'"
-        return 1
