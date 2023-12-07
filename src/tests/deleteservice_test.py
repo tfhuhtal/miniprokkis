0070ec53addcfde056def66a0253d8fc65992773
loeppoe <maria.lipponen@helsinki.fi>
2023-12-07 15:28:13 +0200

    delete unittests

--- src/tests/deleteservice_test.py
+++ src/tests/deleteservice_test.py
@@ -1,6 +1,7 @@
 # pylint: skip-file
 import unittest
 from unittest.mock import Mock
+from services.add import AddService
 from services.delete import DeleteService
 from services.console_io import ConsoleIO
 
@@ -9,6 +10,23 @@ class TestDeleteService(unittest.TestCase):
         self.converter = Mock()
         self.converter.return_data.return_value = []
         self.io = ConsoleIO()
-        self.delete_service = DeleteService(self.converter, self.io)
+        self.adder = AddService(self.converter, self.io)
+        self.deleter = DeleteService(self.converter, self.io)
 
-#    def test_delete_reference(self):
+    def test_delete_reference_validkey(self):
+        reference = Mock()
+        reference.to_json.return_value = {'key': 'ValidKey', 'type': 'book', 'fields': {}}
+        self.adder.add_reference(reference)
+        self.io.inputs = ["ValidKey"]
+        self.deleter.delete()
+        self.assertAlmostEqual(['\nLähde poistettu.'], self.io.outputs)
+    
+    def test_delete_reference_invalidkey(self):
+        self.io.inputs = ["InvalidKey", ""]
+        self.deleter.delete()
+        self.assertEqual(2, len(self.io.outputs))
+
+    def test_delete_reference_cancel(self):
+        self.io.inputs = [""]
+        self.deleter.delete()
+        self.assertAlmostEqual(1, len(self.io.outputs))
