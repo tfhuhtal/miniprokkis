6130208ce0bae9d3a75c99c68abef90e476761a2
Juselius Ville-Petteri <vicjusel@cubbli22-gold.local>
2023-12-11 20:30:21 +0200

    Did some linting

--- src/services/recommend.py
+++ src/services/recommend.py
@@ -25,12 +25,13 @@ class Recommendation:
                 for i in range(len(self.json_data)):
                     if input.lower() in str(self.json_data[i]["key"]).lower():
                         book = self.json_data[i]
-                prompt_text = f"Give me a book recommendation based on the following book: {book['fields']['title']}, {book['fields']['author']}. Just the books name is enough."
+                prompt_text = f"Give me a book recommendation based on the following book:\
+                      {book['fields']['title']}, {book['fields']['author']}. Just the books name is enough."
                 response = self.send_prompt(prompt_text)
                 print("Kirjasuositus:", f"'{response}'")
 
     def send_prompt(self, prompt):
-        with open('src/assets/api_key.txt', 'r') as file:
+        with open('src/assets/api_key.txt', 'r', encoding="utf-8") as file:
             api_key = file.read().strip()
             client = OpenAI(api_key=api_key)
             response = client.chat.completions.create(model="gpt-3.5-turbo",  # Specify the model you want to use
